eb8e2150cbeb7899d17a7771a58fc97fd5bf208e
dubeycode <dubeysatyam3635@gmail.com>
2025-08-10 01:33:59 +0530

    Lowercase

--- textutills/src/components/Textforms.jsx
+++ textutills/src/components/Textforms.jsx
@@ -12,16 +12,38 @@ export const Textforms = (props) => {
         setText(event.target.value);
     }
 
-    const [text,setText]=useState('Enter text here')
+    const handleLowerClick=()=>{
+        // console.log("uppercase was Clicked" +text);
+        let newText=text.toLowerCase();
+        setText(newText)
+    }
+    const handleOnChangeLower=(event)=>{
+        // console.log("On Change")
+        setText(event.target.value);
+    }
+
+    const [text,setText]=useState('')
     
     return (
+       <>
         <div>
             <h1>{props.heading} </h1>
             <div className="mb-3">
-                <textarea  className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+                <textarea  className="form-control" value={text} onChange={handleOnChange} onChang={handleOnChangeLower}  id="mybox" rows="8" placeholder='Enter text here press button and see the magic'></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleupClick}>Convert to uppercase</button>
+            
+            <button className="btn btn-primary"  onClick={handleupClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-3" onClick={handleLowerClick}>Convert to LowerCase</button>
         </div>
+        <div className="container my-3">
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+
+       </>
 
     )
 }
