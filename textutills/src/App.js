a0b97bf501dd9ebbb0fbce433f756fac60294096
dubeycode <dubeysatyam3635@gmail.com>
2025-08-09 23:54:19 +0530

    Initial commit

--- textutills/src/App.js
+++ textutills/src/App.js
@@ -1,17 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
-
+import { Textforms } from "./components/Textforms";
 
 let name = "Dubey Ji";
 function App() {
   return (
-    <>
+    <div>
       <Navbar title="TextUtils" about="About TextUtils" />
-      {/* <Navbar /> */}
-
-    </>
+      <div className="container my-3">
+        <Textforms heading="Enter the text to analyze below" />
+      </div>
+     
+    </div>
   );
 }
 
