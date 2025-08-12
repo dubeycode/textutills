98f95fba0f5e3b7bf9a4778c064176e8b2a470b1
dubeycode <dubeysatyam3635@gmail.com>
2025-08-12 18:32:17 +0530

    alert message show add

--- textutills/src/App.js
+++ textutills/src/App.js
@@ -2,25 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Textforms } from "./components/Textforms";
+import { Alert } from "./components/Alert";
+
 import React, { useState } from 'react';
 
 function App() {
-  const [mode, setMode] = useState('light'); //weather dark mode is enable or not 
+  const [mode, setMode] = useState('light'); //weather dark mode is enable or not
+  const [alert, setAlert] = useState(null)
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+  }
+
   const toggleMode=()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='#042743';
+      showAlert("Dark Mode has been enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light Mode has been enabled","success");
     }
     
   }
   return (
     <>
       <Navbar title="TextUtils" mode={mode} about="About TextUtils" toggleMode={toggleMode} />
-      {/* <Navbar title="TextUtils" mode={mode} /> */}
+      <Alert alert={alert}  / >
       <div className="container my-3">
         <Textforms heading="Enter the text to analyze below" mode={mode}  />
       </div>
