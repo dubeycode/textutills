fe0d32ebd029e3b154d69635ac76b74e5d508a3a
dubeycode <dubeysatyam3635@gmail.com>
2025-08-12 23:12:52 +0530

    Add Router and route

--- textutills/src/App.js
+++ textutills/src/App.js
@@ -1,54 +1,67 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { About } from "./components/About";
 import { Textforms } from "./components/Textforms";
-import  {Alert}  from "./components/Alert";
-
+import { Alert } from "./components/Alert";
 import React, { useState } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
 
 function App() {
   const [mode, setMode] = useState('light'); //weather dark mode is enable or not
   const [alert, setAlert] = useState(null)
 
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null)
-    },2000)
+    }, 2000)
   }
 
-  const toggleMode=()=>{
-    if(mode==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='#042743';
-      showAlert("Dark Mode has been enabled","success");
-      document.title='TextUtils -Dark Mode';
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = 'TextUtils -Dark Mode';
       setInterval(() => {
-        document.title='TextUtils is  Amazing Webpage';
+        document.title = 'TextUtils is  Amazing Webpage';
       }, 2000);
       setInterval(() => {
-        document.title='Install textutills Now';
+        document.title = 'Install textutills Now';
       }, 1500);
-     
+
     }
-    else{
+    else {
       setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light Mode has been enabled","success");
-      document.title='TextUtils -Dark Mode';
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
+      document.title = 'TextUtils -Dark Mode';
     }
-    
+
   }
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} about="About TextUtils" toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Textforms showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}  />
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} about="About" toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={< About />} />
+            <Route exact path="/" element={<Textforms showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}/>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
